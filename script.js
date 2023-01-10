e8f9accec4027124637014bcf154ec7dced3a28f
Fouquereau <thomas.fouquereau@gmail.com>
2023-01-10 19:22:25 +0100

    dark/light mode added

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ window.addEventListener('scroll', () => {
         slidingNewsletter.classList.add('active')
     }
 })
+
+const switcher = document.querySelector('.slider');
+
+switcher.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('darkMode', 'Light Mode');
+    } else {
+        localStorage.setItem('darkMode', 'Dark Mode');
+    }
+
+    const currentTheme = localStorage.getItem('darkMode');
+    const body = document.querySelector('body');
+
+    if (currentTheme === 'Light Mode') {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        document.getElementById("toggle-mode").innerHTML = "Dark Mode";
+
+    } if (currentTheme === 'Dark Mode') {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+        document.getElementById("toggle-mode").innerHTML = "Light Mode";
+    }
+
+})
